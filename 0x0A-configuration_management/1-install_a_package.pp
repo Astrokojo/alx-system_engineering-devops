19fc907f895130a03decd6fa25e7e0a4be4c0482
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-01-19 19:56:49 +0000

    s

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,16 @@
+#!/usr/bin/pup
 # install_flask.pp
-
-package { 'python3-pip':
-  ensure => 'installed',
+package { 'Werkzeug':
+	ensure => '2.1.1',
+	provider => 'pip3',
 }
 
-exec { 'install_flask':
-  command => '/usr/bin/pip3 install flask==2.1.0',
-  path    => '/usr/local/bin:/usr/bin:/bin',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+package { 'python3-pip':
+  ensure => present,
+}
+
+package { 'Flask':
+	ensure => '2.1.0',
+	provider => 'pip3',
+	require => Package['python3-pip'],
 }
