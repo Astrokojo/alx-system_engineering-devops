86699bed54dc04c01adbd2833ad86e771d03e0d3
K.A. Osei-Sarfo <astrokojo@gmail.com>
2024-02-19 17:44:37 +0000

    style(0x15-api): make pycodestyle compliant

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,16 +8,14 @@ import sys
 
 if __name__ == "__main__":
     api_url = 'https://jsonplaceholder.typicode.com/'
-    user_id = {'userId': sys.argv[1]}
-
-    todos_response = requests.get(api_url + "todos", params=user_id)
+    user_id = sys.argv[1]
+    user_id_param = {'userId': user_id}
+    todos_response = requests.get(api_url + "todos", params=user_id_param)
     todos = todos_response.json()
-
-    employee_response = requests.get(api_url + "users/{}".format(sys.argv[1]))
+    employee_response = requests.get(api_url + "users/{}".format(user_id))
     employee = employee_response.json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    completed = [todo.get("title") for todo in todos if todo.get("completed") is True]
     print("Employee {} is done with tasks({}/{}):".format(
         employee.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
-    print(todos_response.url)
